102f8da7c88e69b26c372e4e39530b91a2b08f98
jorgelmarquez94 <jorgelmarquez94@gmail.com>
2019-11-03 16:37:25 -0600

    Javascript Challenge

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,29 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//Prepare container
+let tbody = d3.select('tbody');
+
+function dropData(dataset){
+    //Remember cleaning table body to get fresh data from filter
+    tbody.html('');
+    dataset.forEach((dr) => {
+        let row=tbody.append('tr');
+        Object.values(dr).forEach((value) =>{
+            let cell = row.append('td');
+            cell.text(value);
+        });
+    })
+}
+dropData(tableData)
+
+function dropDownClick(){
+    let selected_date = d3.select('#datetime').property('value');
+    let dataFilter = tableData
+    if(selected_date){
+        dataFilter = dataFilter.filter((row) => row.datetime === selected_date)
+    }
+    dropData(dataFilter)
+}
+
+d3.selectAll('#filter-btn').on('click', dropDownClick);
